f9f5a8b474ba03c16886eeed149280f03eb7fd0e
Sharathguru <vsarath200219@gmail.com>
2025-11-21 21:56:12 +0530

    Resume ATS

--- Backend/app.js
+++ Backend/app.js
@@ -12,7 +12,7 @@ const app=express()
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cors({
-    origin:process.env.CORS_ORIGIN || 'https://ats-resume-three.vercel.app',
+    origin:process.env.CORS_ORIGIN || 'http://localhost:5173',
     credentials:true,
 }))
 
